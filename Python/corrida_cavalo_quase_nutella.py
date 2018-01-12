a4501e163af882c314c26e8fec8704a33f056293
Luis Filipe Kopp <lfkopp@gmail.com>
2018-01-12 20:26:45 -0200

    Update corrida_cavalo_quase_nutella.py

--- Python/corrida_cavalo_quase_nutella.py
+++ Python/corrida_cavalo_quase_nutella.py
@@ -4,30 +4,31 @@ import time
 
 import winsound
 
-winsound.PlaySound('horse.wav', winsound.SND_FILENAME)
 
-
-
-pista = 100
+pista = 120
 cavalos = 8
 pos = [0 for x in range(cavalos)]
+for y in range(cavalos):
+    print("\033[1;%s;%sm%s\033[0m" % (cor,40+y,str(' ' * (pista+3))))
+    print("\033[1;%s;%sm%s\033[0m" % (cor,40+y,str(y+1) +  (' ' * pos[y]) +"0o"+  (' ' * (pista-pos[y]))))
+    print("\033[1;%s;%sm%s\033[0m" % (cor,40+y,str(' ' * (pista+3))))
 
-def espaca(x):
-    caminho = ' '
-    for i in range(x):
-        caminho += ' '
-    return caminho
-    
+
+winsound.PlaySound('C:\_kopp\Dropbox\Filipe\_github\FGV_Programming_Languages\Python\horse.wav', winsound.SND_FILENAME)
+
+
 while max(pos)<pista:
     clear_output(True)
-    pos[random.randint(0,cavalos-1)] += 1
+    pos[random.randint(0,cavalos-1)] += min(pista-3,random.randint(0,3))
     for y in range(cavalos):
         if pos[y]==max(pos):
             cor = 31
             ganhador = y+1
         else:
             cor = 32
-        print("\033[1;%s;%sm%s\033[0m" % (cor,40+y,str(y+1) + k + espaca(pos[y])+"0o"+ espaca(pista-pos[y])))
+        print("\033[1;%s;%sm%s\033[0m" % (cor,40+y,str(' ' * (pista+3))))
+        print("\033[1;%s;%sm%s\033[0m" % (cor,40+y,str(y+1) +  (' ' * pos[y]) +"0o"+  (' ' * (pista-pos[y]))))
+        print("\033[1;%s;%sm%s\033[0m" % (cor,40+y,str(' ' * (pista+3))))
     time.sleep(0.05)
    
     
