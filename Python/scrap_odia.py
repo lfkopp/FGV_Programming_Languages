33a1a495bb4d00bb5081033cbea17bf60e17b716
Luis Filipe Kopp <lfkopp@gmail.com>
2018-01-22 11:59:48 -0200

    Create scrap_odia.py

---
+++ Python/scrap_odia.py
@@ -0,0 +1,5 @@
+### https://www.odia.com.br/
+## https://www.odia.com.br/search/lula/page/170/
+## https://www.odia.com.br/index.php?id=%2Fbusca%2Findex.php&dinamico=1&q=lula
+## https://www.odia.com.br/json/top/
+
