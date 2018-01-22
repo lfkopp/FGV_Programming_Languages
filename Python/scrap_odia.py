5d58c00ad1e126597991f2fe95028cb718db2b4e
Luis Filipe Kopp <lfkopp@gmail.com>
2018-01-22 13:24:56 -0200

    Update scrap_odia.py

--- Python/scrap_odia.py
+++ Python/scrap_odia.py
@@ -2,4 +2,6 @@
 ## https://www.odia.com.br/search/lula/page/170/
 ## https://www.odia.com.br/index.php?id=%2Fbusca%2Findex.php&dinamico=1&q=lula
 ## https://www.odia.com.br/json/top/
+## http://s0.ejesa.ig.com.br/json/odia/rates/
+## http://browse.ejesa.igcorp.com.br/odia.ig.com.br/
 
